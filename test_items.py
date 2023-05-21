e18bd47d1deecc7fbb815eb2bdd5149c3e2ad0ea
Михаил Мурзаев <m.murzaev@timeweb.ru>
2023-05-21 08:36:56 +0300

    stepik-selenium-3.6

--- test_items.py
+++ test_items.py
@@ -6,7 +6,5 @@ from selenium.webdriver.common.by import By
 def test_add_to_cart_button_presence(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(5)
-    add_to_cart_button = browser.find_element(By.XPATH,
-                                              "/html[1]/body[1]/div[2]/div[1]/div[2]/div[2]/article[1]/div[1]/div[2]/form["
-                                              "1]/button[1]")
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert add_to_cart_button is not None, "Add to cart button is not present on the page"
